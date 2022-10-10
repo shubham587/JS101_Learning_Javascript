7db9baa9b92d83bc03471d3cd20d0260e5947759
shubham587 <shubhampwr9945@gmail.com>
2022-10-10 17:38:49 +0530

    Day 1 program added

---
+++ Day_1_Problem/prob2.js
@@ -0,0 +1,6 @@
+var name = "Shubham";
+console.log(name);
+var name = "Balavant";
+console.log(name);
+var name = "Annapurna"
+console.log(name);
---
+++ Day_1_Problem/prob3.js
@@ -0,0 +1,4 @@
+let name = "Shubham";
+let age = 23;
+console.log(name, age);
+console.log(typeof(name),typeof(age));
